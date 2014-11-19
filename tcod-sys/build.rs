a0e01a4c097863702715fca0c56e4af61b9b5c7b
Tomas Sedovic <tomas@sedovic.cz>
2014-11-19 23:57:12 +0100

    Add the tcod-sys crate
    It uses the new build.rs script and exports the headers for libtcod.

---
+++ tcod-sys/build.rs
@@ -0,0 +1,20 @@
+use std::io::fs;
+use std::os;
+
+
+fn main() {
+    let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
+    let dst = Path::new(os::getenv("OUT_DIR").unwrap());
+
+    // TODO: this just copies the precompiled linux lib to the right place. We
+    // want to build it and provide support for other plattforms:
+    let linux_src = src.join("precompiled/libtcod.so");
+    let linux_dst = dst.join("libtcod.so");
+
+    match fs::copy(&linux_src, &linux_dst) {
+        Ok(_) => {
+            println!("cargo:rustc-flags=-l tcod:dylib -L {}", dst.display())
+        }
+        Err(_) => panic!("Copy failed!"),
+    }
+}
