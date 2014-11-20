4cd5a2dcba8692c4af7ec47fb7b009c0901f4a24
Tomas Sedovic <tomas@sedovic.cz>
2014-11-21 00:08:06 +0100

    Convert tabs to spaces in example1

--- examples/example1.rs
+++ examples/example1.rs
@@ -3,16 +3,16 @@ extern crate tcod;
 use tcod::{Console, BackgroundFlag, Key, KeyCode};
 
 fn main() {
-	let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
-	let mut exit = false;
-	while !(Console::window_closed() || exit) {
-		con.clear();
-		con.put_char(40, 25, '@', BackgroundFlag::Set);
-		Console::flush();
-		let keypress = Console::wait_for_keypress(true);
-		match keypress.key {
-			Key::Special(KeyCode::Escape) => exit = true,
-			_ => {}
-		}
-	}
+    let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
+    let mut exit = false;
+    while !(Console::window_closed() || exit) {
+        con.clear();
+        con.put_char(40, 25, '@', BackgroundFlag::Set);
+        Console::flush();
+        let keypress = Console::wait_for_keypress(true);
+        match keypress.key {
+            Key::Special(KeyCode::Escape) => exit = true,
+            _ => {}
+        }
+    }
 }
