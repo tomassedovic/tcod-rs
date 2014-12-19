8bce6e30810ac23d8842a53a26f3295a2f2d5539
Tomas Sedovic <tomas@sedovic.cz>
2014-12-20 00:18:55 +0100

    Make the build script work
    Seems to work on Linux and Mac OS X.

--- tcod-sys/build.rs
+++ tcod-sys/build.rs
@@ -1,19 +1,46 @@
-use std::io::fs;
+use std::io::{fs, Command};
+use std::io::process::InheritFd;
 use std::os;
 
 
 fn main() {
-    panic!("cannot compile");
     let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
     let dst = Path::new(os::getenv("OUT_DIR").unwrap());
 
-    // TODO: this just copies the precompiled linux lib to the right place. We
-    // want to build it and provide support for other plattforms:
-    let linux_src = src.join("precompiled/libtcod.so");
-    let linux_dst = dst.join("libtcod.so");
+    let target = os::getenv("TARGET").unwrap();
+    let (makefile, dylib_ext) = if target.contains("linux") {
+        ("makefile-linux", "so")
+    } else if target.contains("darwin") {
+        ("makefile-osx", "dylib")
+    } else {
+        panic!("Don't know how to handle target {} yet.", target);
+    };
 
-    match fs::copy(&linux_src, &linux_dst) {
+    let libtcod_dir = src.join("libtcod");
+
+    let mut make = Command::new("make");
+    make.arg("-f")
+        .arg(Path::new("makefiles").join(makefile))
+        .arg("clean")
+        .arg("all")
+        .cwd(&libtcod_dir);
+    make.stdout(InheritFd(1))
+        .stdout(InheritFd(2))
+        .status()
+        .unwrap()
+        .success();
+
+
+    let lib_name = Path::new("libtcod").with_extension(dylib_ext);
+    match fs::copy(&libtcod_dir.join(&lib_name), &dst.join(&lib_name)) {
         Ok(_) => {
+            if target.contains("linux") {
+                // TODO(tsedovic): code using this seems to expect
+                // `libtcod.so.1` instead of `libtcod.so` so just copy that,
+                // too. No idea why?
+                fs::copy(&libtcod_dir.join(&lib_name),
+                         &dst.join(&lib_name.with_extension("so.1"))).unwrap();
+            }
             println!("cargo:rustc-flags=-l tcod:dylib -L {}", dst.display())
         }
         Err(_) => panic!("Copy failed!"),
