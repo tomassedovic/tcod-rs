fd1c5af2532021153009f55f088304cafcfe33d3
Tomas Sedovic <tomas@sedovic.cz>
2014-12-21 15:20:05 +0100

    Add Windows support to tcod-sys' build script

--- tcod-sys/build.rs
+++ tcod-sys/build.rs
@@ -8,10 +8,12 @@ fn main() {
     let dst = Path::new(os::getenv("OUT_DIR").unwrap());
 
     let target = os::getenv("TARGET").unwrap();
-    let (makefile, dylib_ext) = if target.contains("linux") {
-        ("makefile-linux", "so")
+    let makefile = if target.contains("linux") {
+        "makefile-linux"
     } else if target.contains("darwin") {
-        ("makefile-osx", "dylib")
+        "makefile-osx"
+    } else if target.contains("windows") {
+        "makefile-mingw"
     } else {
         panic!("Don't know how to handle target {} yet.", target);
     };
@@ -24,25 +26,32 @@ fn main() {
         .arg("clean")
         .arg("all")
         .cwd(&libtcod_dir);
-    make.stdout(InheritFd(1))
-        .stdout(InheritFd(2))
-        .status()
-        .unwrap()
-        .success();
+    assert!(make.stdout(InheritFd(1))
+            .stdout(InheritFd(2))
+            .status()
+            .unwrap()
+            .success());
 
-
-    let lib_name = Path::new("libtcod").with_extension(dylib_ext);
-    match fs::copy(&libtcod_dir.join(&lib_name), &dst.join(&lib_name)) {
-        Ok(_) => {
-            if target.contains("linux") {
-                // TODO(tsedovic): code using this seems to expect
-                // `libtcod.so.1` instead of `libtcod.so` so just copy that,
-                // too. No idea why?
-                fs::copy(&libtcod_dir.join(&lib_name),
-                         &dst.join(&lib_name.with_extension("so.1"))).unwrap();
-            }
-            println!("cargo:rustc-flags=-l tcod:dylib -L {}", dst.display())
-        }
-        Err(_) => panic!("Copy failed!"),
+    // TODO(shadower): there's a bunch of name special-casing I came across
+    // testing this in my linux and windows setups. I have no idea why these are
+    // required, so right now we just produce the compiled binary in all the
+    // names necessary. There's probably a better way to deal with this.
+    if target.contains("linux") {
+        fs::copy(&libtcod_dir.join("libtcod.so"), &dst.join("libtcod.so")).unwrap();
+        // NOTE(shadower): this is what the executables seem to expect:
+        fs::copy(&libtcod_dir.join("libtcod.so"), &dst.join("libtcod.so.1")).unwrap();
+    } else if target.contains("darwin") {
+        fs::copy(&libtcod_dir.join("libtcod.dylib"), &dst.join("libtcod.dylib")).unwrap();
+    } else if target.contains("windows") {
+        // NOTE: this seems to be needed by MinGW's gcc:
+        fs::copy(&libtcod_dir.join("libtcod-mingw.dll"), &dst.join("libtcod.dll")).unwrap();
+        // NOTE: this seems to be what the executable using tcod seems to expect:
+        fs::copy(&libtcod_dir.join("libtcod-mingw.dll"), &dst.join("libtcod-mingw.dll")).unwrap();
+        // NOTE: this is shipped with libtcod and Windows executables expect it:
+        fs::copy(&libtcod_dir.join("SDL.dll"), &dst.join("SDL.dll")).unwrap();
     }
+
+    // TODO: can we (optionally?) produce a static copy? It'd probably be more
+    // work, but would be easier for the end users.
+    println!("cargo:rustc-flags=-l tcod:dylib -L {}", dst.display())
 }
