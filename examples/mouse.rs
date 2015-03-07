c4343133fbe71896590442c9cd892f3d02b7db50
Bastien Léonard <bastien.leonard@gmail.com>
2015-03-07 16:02:07 +0100

    Fixed the mouse example

--- examples/mouse.rs
+++ examples/mouse.rs
@@ -11,20 +11,21 @@ fn main() {
     while !Console::window_closed() {
 
         loop {
-            let (_flags, event) = tcod::system::check_for_event(
-                tcod::KEY | tcod::MOUSE);
-
-            match event {
-                tcod::system::Event::Key(ref key_state) => {
-                    println!("{:?}", key_state);
-                },
-                tcod::system::Event::Mouse(ref mouse_state) => {
-                    x = mouse_state.cx as i32;
-                    y = mouse_state.cy as i32;
-                    println!("{:?}", mouse_state);
-                }
-                tcod::system::Event::None => {
+            match tcod::system::check_for_event(tcod::KEY | tcod::MOUSE) {
+                None => {
                     break;
+                }
+                Some((flags, event)) => {
+                    match event {
+                        tcod::system::Event::Key(ref key_state) => {
+                            println!("{:?}", key_state);
+                        },
+                        tcod::system::Event::Mouse(ref mouse_state) => {
+                            x = mouse_state.cx as i32;
+                            y = mouse_state.cy as i32;
+                            println!("{:?}", mouse_state);
+                        }
+                    }
                 }
             }
         }
