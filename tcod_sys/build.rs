86b8e1cffa294a672efdb20165a8502fd65666b7
Hans W. Uhlig <hans.uhlig@ibm.com>
2017-08-29 11:44:32 -0700

    Updating tcod-sys build.rs file to match new gcc-rs api.

--- tcod_sys/build.rs
+++ tcod_sys/build.rs
@@ -7,28 +7,28 @@ use std::path::{Path, PathBuf};
 
 
 fn build_libz(libz_sources: &[&str]) {
-    let mut config = gcc::Config::new();
+    let mut build_config = gcc::Build::new();
     for c_file in libz_sources {
-        config.file(c_file);
+        build_config.file(c_file);
     }
-    config.flag("-w");
-    config.compile("libz.a");
+    build_config.flag("-w");
+    build_config.compile("libz.a");
 }
 
-fn build_libtcod_objects(mut config: gcc::Config, sources: &[&str]) {
-    config.include("libtcod/include");
-    config.include("libtcod/src/zlib");
+fn build_libtcod_objects(mut build_config: gcc::Build, sources: &[&str]) {
+    build_config.include("libtcod/include");
+    build_config.include("libtcod/src/zlib");
     for c_file in sources {
-        config.file(c_file);
+        build_config.file(c_file);
     }
-    config.cargo_metadata(false);
-    config.flag("-w");
-    config.compile("libtcod.a");
+    build_config.cargo_metadata(false);
+    build_config.flag("-w");
+    build_config.compile("libtcod.a");
 }
 
 
-fn compile_config(config: gcc::Config) {
-    let mut cmd = config.get_compiler().to_command();
+fn compile_build_config(build_config: gcc::Build) {
+    let mut cmd = build_config.get_compiler().to_command();
     println!("Compiling: {:?}", cmd);
     match cmd.output() {
         Ok(output) => {
@@ -63,52 +63,52 @@ fn main() {
 
     let libz_sources = &[
         "libtcod/src/zlib/adler32.c",
-	    "libtcod/src/zlib/crc32.c",
-	    "libtcod/src/zlib/deflate.c",
-	    "libtcod/src/zlib/infback.c",
-	    "libtcod/src/zlib/inffast.c",
-	    "libtcod/src/zlib/inflate.c",
-	    "libtcod/src/zlib/inftrees.c",
-	    "libtcod/src/zlib/trees.c",
-	    "libtcod/src/zlib/zutil.c",
-	    "libtcod/src/zlib/compress.c",
-	    "libtcod/src/zlib/uncompr.c",
-	    "libtcod/src/zlib/gzclose.c",
-	    "libtcod/src/zlib/gzlib.c",
-	    "libtcod/src/zlib/gzread.c",
-	    "libtcod/src/zlib/gzwrite.c",
+        "libtcod/src/zlib/crc32.c",
+        "libtcod/src/zlib/deflate.c",
+        "libtcod/src/zlib/infback.c",
+        "libtcod/src/zlib/inffast.c",
+        "libtcod/src/zlib/inflate.c",
+        "libtcod/src/zlib/inftrees.c",
+        "libtcod/src/zlib/trees.c",
+        "libtcod/src/zlib/zutil.c",
+        "libtcod/src/zlib/compress.c",
+        "libtcod/src/zlib/uncompr.c",
+        "libtcod/src/zlib/gzclose.c",
+        "libtcod/src/zlib/gzlib.c",
+        "libtcod/src/zlib/gzread.c",
+        "libtcod/src/zlib/gzwrite.c",
     ];
 
     let libtcod_sources = &[
- 	    "libtcod/src/bresenham_c.c",
-	    "libtcod/src/bsp_c.c",
-	    "libtcod/src/color_c.c",
-	    "libtcod/src/console_c.c",
-	    "libtcod/src/fov_c.c",
-	    "libtcod/src/fov_circular_raycasting.c",
-	    "libtcod/src/fov_diamond_raycasting.c",
-	    "libtcod/src/fov_recursive_shadowcasting.c",
-	    "libtcod/src/fov_permissive2.c",
-	    "libtcod/src/fov_restrictive.c",
-	    "libtcod/src/heightmap_c.c",
-	    "libtcod/src/image_c.c",
-	    "libtcod/src/lex_c.c",
-	    "libtcod/src/list_c.c",
-	    "libtcod/src/mersenne_c.c",
-	    "libtcod/src/noise_c.c",
-	    "libtcod/src/parser_c.c",
-	    "libtcod/src/path_c.c",
-	    "libtcod/src/sys_c.c",
-	    "libtcod/src/sys_opengl_c.c",
-	    "libtcod/src/sys_sdl_c.c",
-	    "libtcod/src/sys_sdl_img_bmp.c",
-	    "libtcod/src/sys_sdl_img_png.c",
-	    "libtcod/src/tree_c.c",
-	    "libtcod/src/txtfield_c.c",
-	    "libtcod/src/wrappers.c",
-	    "libtcod/src/zip_c.c",
-	    "libtcod/src/namegen_c.c",
-	    "libtcod/src/png/lodepng.c",
+        "libtcod/src/bresenham_c.c",
+        "libtcod/src/bsp_c.c",
+        "libtcod/src/color_c.c",
+        "libtcod/src/console_c.c",
+        "libtcod/src/fov_c.c",
+        "libtcod/src/fov_circular_raycasting.c",
+        "libtcod/src/fov_diamond_raycasting.c",
+        "libtcod/src/fov_recursive_shadowcasting.c",
+        "libtcod/src/fov_permissive2.c",
+        "libtcod/src/fov_restrictive.c",
+        "libtcod/src/heightmap_c.c",
+        "libtcod/src/image_c.c",
+        "libtcod/src/lex_c.c",
+        "libtcod/src/list_c.c",
+        "libtcod/src/mersenne_c.c",
+        "libtcod/src/noise_c.c",
+        "libtcod/src/parser_c.c",
+        "libtcod/src/path_c.c",
+        "libtcod/src/sys_c.c",
+        "libtcod/src/sys_opengl_c.c",
+        "libtcod/src/sys_sdl_c.c",
+        "libtcod/src/sys_sdl_img_bmp.c",
+        "libtcod/src/sys_sdl_img_png.c",
+        "libtcod/src/tree_c.c",
+        "libtcod/src/txtfield_c.c",
+        "libtcod/src/wrappers.c",
+        "libtcod/src/zip_c.c",
+        "libtcod/src/namegen_c.c",
+        "libtcod/src/png/lodepng.c",
     ];
 
     if target.contains("linux") {
@@ -116,74 +116,72 @@ fn main() {
 
         // Build the *.o files:
         {
-            let mut config = gcc::Config::new();
-            config.flag("-fno-strict-aliasing");
-            config.flag("-ansi");
-            build_libtcod_objects(config, libtcod_sources);
+            let mut build_config = gcc::Build::new();
+            build_config.flag("-fno-strict-aliasing");
+            build_config.flag("-ansi");
+            build_libtcod_objects(build_config, libtcod_sources);
         }
 
         // Build the DLL
-        let mut config = gcc::Config::new();
-        config.flag("-shared");
-        config.flag("-Wl,-soname,libtcod.so");
-        config.flag("-o");
-        config.flag(dst.join("libtcod.so").to_str().unwrap());
+        let mut build_config = gcc::Build::new();
+        build_config.flag("-shared");
+        build_config.flag("-Wl,-soname,libtcod.so");
+        build_config.flag("-o");
+        build_config.flag(dst.join("libtcod.so").to_str().unwrap());
         for c_file in libtcod_sources {
-            config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
+            build_config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
         }
-        config.flag(dst.join("libz.a").to_str().unwrap());
-        config.flag("-lSDL");
-        config.flag("-lGL");
-        config.flag("-lX11");
-        config.flag("-lm");
-        config.flag("-ldl");
-        config.flag("-lpthread");
+        build_config.flag(dst.join("libz.a").to_str().unwrap());
+        build_config.flag("-lSDL");
+        build_config.flag("-lGL");
+        build_config.flag("-lX11");
+        build_config.flag("-lm");
+        build_config.flag("-ldl");
+        build_config.flag("-lpthread");
 
-        compile_config(config);
+        compile_build_config(build_config);
         assert!(dst.join("libtcod.so").is_file());
 
         pkg_config::find_library("sdl").unwrap();
         pkg_config::find_library("gl").unwrap();
         pkg_config::find_library("x11").unwrap();
-
     } else if target.contains("darwin") {
         build_libz(libz_sources);
 
         // Build the *.o files
         {
-            let mut config = gcc::Config::new();
-            config.flag("-fno-strict-aliasing");
-            config.flag("-ansi");
-            build_libtcod_objects(config, libtcod_sources);
+            let mut build_config = gcc::Build::new();
+            build_config.flag("-fno-strict-aliasing");
+            build_config.flag("-ansi");
+            build_libtcod_objects(build_config, libtcod_sources);
         }
 
         // Build the DLL
-        let mut config = gcc::Config::new();
-        config.flag("-shared");
-        config.flag("-o");
-        config.flag(dst.join("libtcod.dylib").to_str().unwrap());
+        let mut build_config = gcc::Build::new();
+        build_config.flag("-shared");
+        build_config.flag("-o");
+        build_config.flag(dst.join("libtcod.dylib").to_str().unwrap());
         for c_file in libtcod_sources {
-            config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
+            build_config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
         }
-        config.flag(dst.join("libz.a").to_str().unwrap());
-        config.flag(src.join("libtcod/osx/macsupport.m").to_str().unwrap());
-        config.flag("-lSDL");
-        config.flag("-lSDLmain");
-        config.flag("-framework");
-        config.flag("OpenGL");
-        config.flag("-framework");
-        config.flag("Cocoa");
-        config.flag("-lm");
-        config.flag("-ldl");
-        config.flag("-lpthread");
+        build_config.flag(dst.join("libz.a").to_str().unwrap());
+        build_config.flag(src.join("libtcod/osx/macsupport.m").to_str().unwrap());
+        build_config.flag("-lSDL");
+        build_config.flag("-lSDLmain");
+        build_config.flag("-framework");
+        build_config.flag("OpenGL");
+        build_config.flag("-framework");
+        build_config.flag("Cocoa");
+        build_config.flag("-lm");
+        build_config.flag("-ldl");
+        build_config.flag("-lpthread");
 
-        compile_config(config);
+        compile_build_config(build_config);
         assert!(dst.join("libtcod.dylib").is_file());
 
         pkg_config::find_library("sdl").unwrap();
         println!("cargo:rustc-link-lib=framework=OpenGL");
         println!("cargo:rustc-link-lib=framework=Cocoa");
-
     } else if target.contains("windows-gnu") {
         assert!(sdl_lib_dir.is_dir());
         assert!(sdl_include_dir.is_dir());
@@ -193,41 +191,40 @@ fn main() {
 
         // Build the *.o files:
         {
-            let mut config = gcc::Config::new();
-            config.include(sdl_include_dir.to_str().unwrap());
-            config.flag("-fno-strict-aliasing");
-            config.flag("-ansi");
-            config.define("LIBTCOD_EXPORTS", None);
-            build_libtcod_objects(config, libtcod_sources);
+            let mut build_config = gcc::Build::new();
+            build_config.include(sdl_include_dir.to_str().unwrap());
+            build_config.flag("-fno-strict-aliasing");
+            build_config.flag("-ansi");
+            build_config.define("LIBTCOD_EXPORTS", None);
+            build_libtcod_objects(build_config, libtcod_sources);
         }
 
         // Build the DLL
-        let mut config = gcc::Config::new();
-        config.flag("-o");
-        config.flag(dst.join("libtcod.dll").to_str().unwrap());
-        config.flag("-shared");
+        let mut build_config = gcc::Build::new();
+        build_config.flag("-o");
+        build_config.flag(dst.join("libtcod.dll").to_str().unwrap());
+        build_config.flag("-shared");
         fs::create_dir(dst.join("lib")).unwrap();
-        config.flag(&format!("-Wl,--out-implib,{}", dst.join("lib/libtcod.a").display()));
+        build_config.flag(&format!("-Wl,--out-implib,{}", dst.join("lib/libtcod.a").display()));
         for c_file in libtcod_sources {
-            config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
+            build_config.flag(dst.join(c_file).with_extension("o").to_str().unwrap());
         }
-        config.flag(dst.join("libz.a").to_str().unwrap());
-        config.flag("-mwindows");
-        config.flag("-L");
-        config.flag(sdl_lib_dir.to_str().unwrap());
-        config.flag("-lSDL.dll");
-        config.flag("-lopengl32");
-        config.flag("-static-libgcc");
-        config.flag("-static-libstdc++");
+        build_config.flag(dst.join("libz.a").to_str().unwrap());
+        build_config.flag("-mwindows");
+        build_config.flag("-L");
+        build_config.flag(sdl_lib_dir.to_str().unwrap());
+        build_config.flag("-lSDL.dll");
+        build_config.flag("-lopengl32");
+        build_config.flag("-static-libgcc");
+        build_config.flag("-static-libstdc++");
 
-        compile_config(config);
+        compile_build_config(build_config);
         assert!(dst.join("libtcod.dll").is_file());
 
         println!("cargo:rustc-link-lib=dylib={}", "SDL");
         println!("cargo:rustc-link-lib=dylib={}", "opengl32");
         println!("cargo:rustc-link-search=native={}", sdl_lib_dir.display());
         println!("cargo:rustc-link-search=native={}", dst.display());
-
     } else if target.contains("windows-msvc") {
         assert!(sdl_lib_dir.is_dir());
         assert!(sdl_include_dir.is_dir());
@@ -236,28 +233,28 @@ fn main() {
         fs::copy(&sdl_lib_dir.join("SDLmain.lib"), &dst.join("SDLmain.lib")).unwrap();
 
         // Build the DLL
-        let mut config = gcc::Config::new();
-        config.flag("/DLIBTCOD_EXPORTS");
-        config.flag("/DNO_OPENGL");
-        config.include(sdl_include_dir.to_str().unwrap());
-        config.include(Path::new("libtcod").join("src").join("zlib"));
-        config.include(Path::new("libtcod").join("include"));
+        let mut build_config = gcc::Build::new();
+        build_config.flag("/DLIBTCOD_EXPORTS");
+        build_config.flag("/DNO_OPENGL");
+        build_config.include(sdl_include_dir.to_str().unwrap());
+        build_config.include(Path::new("libtcod").join("src").join("zlib"));
+        build_config.include(Path::new("libtcod").join("include"));
         for c_file in libz_sources.iter().chain(libtcod_sources) {
             // Make sure the path is in the Windows format. This
             // shouldn't matter but it's distracting when debugging
             // build script issues.
             let path = c_file.split('/').fold(PathBuf::new(), |path, segment| path.join(segment));
-            config.flag(src.join(path).to_str().unwrap());
+            build_config.flag(src.join(path).to_str().unwrap());
         }
-        config.flag("User32.lib");
-        config.flag("SDL.lib");
-        config.flag("SDLmain.lib");
-        config.flag("/link");
-        config.flag(&format!("/LIBPATH:{}", dst.to_str().unwrap()));
-        config.flag("/DLL");
-        config.flag(&format!("/OUT:{}", dst.join("tcod.dll").display()));
+        build_config.flag("User32.lib");
+        build_config.flag("SDL.lib");
+        build_config.flag("SDLmain.lib");
+        build_config.flag("/link");
+        build_config.flag(&format!("/LIBPATH:{}", dst.to_str().unwrap()));
+        build_config.flag("/DLL");
+        build_config.flag(&format!("/OUT:{}", dst.join("tcod.dll").display()));
 
-        compile_config(config);
+        compile_build_config(build_config);
         assert!(dst.join("tcod.dll").is_file());
 
         println!("cargo:rustc-link-search={}", dst.display());
